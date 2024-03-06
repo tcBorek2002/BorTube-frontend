a4cf5b9dc4824220cfd102ba8bf6dc01e7009bba
Borek Bandell <borekbandell@hotmail.com>
2024-03-06 17:29:09 +0100

    Made video card use parent data

--- lib/widgets/videos/video_card.dart
+++ lib/widgets/videos/video_card.dart
@@ -1,6 +1,13 @@
 import 'package:bortube_frontend/objects/video.dart';
 import 'package:flutter/material.dart';
 
+String formatDuration(int seconds) {
+  Duration duration = Duration(seconds: seconds);
+  String formattedDuration =
+      '${duration.inMinutes.remainder(60)}:${(duration.inSeconds.remainder(60)).toString().padLeft(2, '0')}';
+  return formattedDuration;
+}
+
 class VideoCard extends StatelessWidget {
   const VideoCard({super.key, required this.video});
 
@@ -8,18 +15,36 @@ class VideoCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Card(
-        child: Column(
-          children: [
-            Image(
-              image: AssetImage('assets/thumbnail.png'),
-              height: 170,
-            ),
-            Row(
-              children: [Text("Title"), Text("10:00")],
-            )
-          ],
+    return SizedBox(
+      width: 300,
+      child: Center(
+        child: Card(
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: const Image(
+                  image: AssetImage('assets/thumbnail.png'),
+                  height: 170,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: SizedBox(
+                  child: Row(
+                    children: [
+                      Text(
+                        video.title,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const Spacer(),
+                      Text(formatDuration(video.duration))
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
